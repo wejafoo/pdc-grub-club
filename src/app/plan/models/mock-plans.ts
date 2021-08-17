2fe48563a2e77ceaa81f8c2490cc3aac52e7539a
wejafoo <wes@weja.us>
2021-08-17 16:48:41 -0600

    clean up routing and navigation by collapsing deep nesting of routes   * Some light style standardization

--- src/app/plan/models/mock-plans.ts
+++ src/app/plan/models/mock-plans.ts
@@ -3,8 +3,15 @@
 import { Plan } from './plan';
 
 export const PLANS: Plan[] = [
-	{ id: 1, name: '2021'		},
-	{ id: 2, name: '2022'		},
-	{ id: 3, name: '2023'		},
-	{ id: 4, name: '2021-test'	}
+	{ id: 1, name: 'Fall - 2021', events: [
+		{ id: 1, name: 'October'	},
+		{ id: 2, name: 'November'	}
+	]},
+	{ id: 2, name: 'Spring - 2022', events: [
+		{ id: 1, name: 'January'	},
+		{ id: 2, name: 'February'	},
+		{ id: 3, name: 'March'		}
+	]},
+	{ id: 3, name: 'Spring - 2023',	events: [{id: 1, name: 'January'}]},
+	{ id: 4, name: '2020-test',		events: [{id: 1, name: 'January'}]}
 ];
