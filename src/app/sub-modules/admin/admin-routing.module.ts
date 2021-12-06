fd226156ce1a9e29d93d146d73d96833a884d9f8
wejafoo <wes@weja.us>
2021-12-06 15:40:12 -0700

    Clean up UI across all pages   * standardize nav bars, buttons, links, colors, etc

--- src/app/sub-modules/admin/admin-routing.module.ts
+++ src/app/sub-modules/admin/admin-routing.module.ts
@@ -7,14 +7,14 @@ import { AdminComponent				} from './admin/admin.component';
 import { AdminDashboardComponent	} from './admin-dashboard/admin-dashboard.component';
 import { ManagePlansComponent		} from './manage-plans/manage-plans.component';
 import { ManagePresbiesComponent	} from './manage-presbies/manage-presbies.component';
-import { AuthGuard					} from '../auth/services/auth.guard';
+import { AuthGuard					} from '../../_guards/auth.guard';
 
 const adminRoutes: Routes = [
 	{ path: '', component: AdminComponent, canActivate: [AuthGuard], children: [
 		{ path: '', canActivateChild: [AuthGuard], children: [
-				{ path: 'plans',	component: ManagePlansComponent	},
-				{ path: 'presbies',	component: ManagePresbiesComponent	},
-				{ path: '',			component: AdminDashboardComponent	}
+			{ path: 'plans',	component: ManagePlansComponent		},
+			{ path: 'presbies',	component: ManagePresbiesComponent	},
+			{ path: '',			component: AdminDashboardComponent	}
 		]}
 	]}
 ];
